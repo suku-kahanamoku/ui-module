40eeb7650e3e333d474fb6754e15051deabd1760
Suche Kahanamoku <sukusovi@gmail.com>
2025-07-19 12:52:44 +0200

    first commit

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
-  myModule: {},
+  modules: ["../src/module"],
+  uiModule: {},
   devtools: { enabled: true },
-})
+});
--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,7 +1,5 @@
-import MyModule from '../../../src/module'
+import UiModule from "../../../src/module";
 
 export default defineNuxtConfig({
-  modules: [
-    MyModule,
-  ],
-})
+  modules: [UiModule],
+});
