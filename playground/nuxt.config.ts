fe37e10d9aeb36247622f30a68e7aa1913c578e5
Suku <sukusovi@gmail.com>
2026-05-04 18:29:07 +0200

    Update.

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,4 +2,9 @@ export default defineNuxtConfig({
   modules: ["../src/module"],
   uiModule: {},
   devtools: { enabled: true },
+  vite: {
+    optimizeDeps: {
+      include: ["@vue/devtools-core", "@vue/devtools-kit"],
+    },
+  },
 });
